d614a280b88e5b83aa6b74d9f0bdf2bd2bad5c81
Tom Romeo <Tom.Romeo.Doil@gmail.com>
2022-06-16 12:06:51 +0200

    add: frontend skeleton

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,15 @@
+
+<div id="container">
+    <h1>WhoAmI</h1>
+</div>
+
+<style lang="scss">
+    #container {
+        width: 100%;
+        height: 100vh;
+        
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
